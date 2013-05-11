cf57ce62fdb93d8362c3012125ee4ade864a5ab7
msn_7980 <msn_7980@hotmail.com>
2013-05-11 10:50:41 -0300

    ...

---
+++ start/start.bat
@@ -0,0 +1,3 @@
+start cmd /C "@jboss_ejb@\bin\standalone.bat"
+start cmd /C "@jboss_web@\bin\standalone.bat"
+start cmd /C "@jboss_ws@\bin\standalone.bat"
---
+++ start/start.sh
@@ -0,0 +1,3 @@
+terminal -e @jboss_ejb@/bin/standalone.sh &
+terminal -e @jboss_web@/bin/standalone.sh &
+terminal -e @jboss_ws@/bin/standalone.sh &
